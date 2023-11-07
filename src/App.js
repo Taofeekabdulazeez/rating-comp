bfd6377d549120b92eb26040ade105a516e93b7f
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2023-11-07 19:30:34 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div className="container">
+      <Logo />
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div>
+      <span className="logo">⭐</span>
+    </div>
+  );
+}
